a44f7d4a0fe6ba7e76f6dcaf2c897e2d01fda561
IzarbeBailoSanValero <a28303@svalero.com>
2026-02-24 15:16:52 +0100

    configurado interceptor y axios

--- perpetuum-frontend-vuetify-proyect/src/App.vue
+++ perpetuum-frontend-vuetify-proyect/src/App.vue
@@ -7,6 +7,6 @@
 <script setup lang="ts">
 /*
 Apuntes: 
-Aquí no debe haber lógica, porque:Es el punto de entrada.Solo envuelve todo en <v-app>.El router decide qué layout mostrar.
+- Aquí no debe haber lógica, porque:Es el punto de entrada.Solo envuelve todo en <v-app>.El router decide qué layout mostrar.
 */
 </script>
---
+++ perpetuum-frontend-vuetify-proyect/src/plugins/axios.ts
@@ -0,0 +1,30 @@
+/*
+- Apuntes sobre tuberías: conexiónc on el back:
+      - Axios: El cartero que lleva y trae mensajes entre Vue y .NET.
+      - Pinia Store: El almacén donde guardamos lo que el cartero nos trae.
+      - Componente Vue: El encargado de pintar esos datos usando Vuetify.
+
+Pasos: 
+1º centralizamos la conexión. Si mañana cambias el puerto de tu Backend, solo lo tocas aquí.
+ */
+
+import axios from 'axios'
+
+const apiClient = axios.create({
+  // URL base API en .NET (hay que ajustar el puerto según  launchSettings.json)
+  baseURL: 'https://localhost:5000/api', 
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// Interceptor: Antes de cada petición, "inyectamos" el token si existe
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default apiClient
