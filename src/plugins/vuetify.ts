041b8481a44dab2cb2c816373eb6ee62fef9bf7b
IzarbeBailoSanValero <a28303@svalero.com>
2026-03-01 12:04:48 +0100

    corrección ene stilo

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -14,6 +14,22 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#6200EE',
+          secondary: '#03DAC6',
+          error: '#B00020',
+        },
+      },
+      dark: {
+        colors: {
+          primary: '#BB86FC',
+          secondary: '#03DAC6',
+          error: '#CF6679',
+        },
+      },
+    },
   },
 })
