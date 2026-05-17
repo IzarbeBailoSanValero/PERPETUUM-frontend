bbe90c41b3d7dcb36d8b0eb39d3630ebb4a7e151
IzarbeBailoSanValero <a28303@svalero.com>
2026-05-17 15:15:42 +0200

    formateado de fecha a humano. uso el locale del navegador del usuario. modo español

--- src/components/user/MyMemoryCard.vue
+++ src/components/user/MyMemoryCard.vue
@@ -5,12 +5,15 @@ const props = defineProps<{ memory: Memory }>()
 
 const emit = defineEmits<{ delete: [memoryId: number] }>()
 
+const dateFormatted = new Date(props.memory.createdDate).toLocaleDateString('es-ES')
+
+
 </script>
 
 <template>
   <div>
     <h3><strong>Recuerdo dedicado a {{ memory.deceasedName }}</strong></h3>
-    <h4>Fecha: {{ memory.createdDate }}</h4>
+    <h4>Escrito el {{dateFormatted}}</h4>
     <img v-if="memory.type === 'Photo' && memory.mediaURL" :src="memory.mediaURL" alt="foto subida">
     <h5 v-if="memory.textContent">{{ memory.textContent }}</h5>
     <p>Estado: {{ memory.status }}</p>
